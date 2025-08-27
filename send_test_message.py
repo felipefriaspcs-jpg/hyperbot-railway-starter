e7c137b839a30ef6ebc41f2f1f9201f460b1e991
felipefriaspcs-jpg <felipe.frias.pcs@gmail.com>
2025-08-27 02:19:41 -0600

    Update send_test_message.py

--- send_test_message.py
+++ send_test_message.py
@@ -1,14 +1,13 @@
 import requests
 
 def send_telegram_message():
-    # 🚨 Hardcoded for testing only — delete after confirming it's working
     token = "8282101491:AAGgnEF-ML2Jv0dnBF76No1kzCyIdo6A9vU"
-    user_id = "1404344692"
+    chat_id = "1404344692"
     message = "🚀 HyperBot está en línea, mi comandante!"
 
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     payload = {
-        "chat_id": Flip42022,
+        "chat_id": chat_id,
         "text": message
     }
 
